3a0e1acff377d96ebf0698bb9998f808c7911816
25sprout-alan <alan@25sprout.com>
2020-03-16 04:39:59 +0800

    fix: Add launch screen image.

--- src/configs/detectDevice.js
+++ src/configs/detectDevice.js
@@ -4,9 +4,23 @@ const detectDevice = () => {
     window.isMobileDevice = true;
   }
 
-  if (window.matchMedia('(display-mode: fullscreen)').matches) {
+  if (window.matchMedia('(display-mode: standalone)').matches) {
     window.isPwa = true;
   }
 };
+
+// Detects if device is on iOS
+export const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test(userAgent);
+};
+
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
 
 export default detectDevice;
