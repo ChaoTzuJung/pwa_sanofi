ce8f8266f9e2b53971c6b4f464867f5ed687e112
25sprout-alan <alan@25sprout.com>
2020-03-25 01:23:12 +0800

    fix: Fix store module issue

--- src/pages/Calculator.vue
+++ src/pages/Calculator.vue
@@ -1,8 +1,6 @@
 <script>
 import Footer from 'components/Common/Footer.vue';
 import CalculatorLayout from '@/layouts/Calculator/index.vue';
-import calculatorModule from '@/store/modules/calculator';
-import patientModule from '@/store/modules/patient';
 
 export default {
   name: 'CalculatorPage',
@@ -12,14 +10,6 @@ export default {
   },
   created() {
     window.scrollTo(0, 0);
-  },
-  mounted() {
-    if (!this.$store.hasModule('calculator')) {
-      this.$store.registerModule('calculator', calculatorModule());
-    }
-    if (!this.$store.hasModule('patient')) {
-      this.$store.registerModule('patient', patientModule());
-    }
   },
 };
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,22 @@
 <script>
 import Footer from 'components/Common/Footer.vue';
 import HomeLayout from '@/layouts/Home/index.vue';
+import calculatorModule from '@/store/modules/calculator';
+import patientModule from '@/store/modules/patient';
 
 export default {
   name: 'HomePage',
   components: {
     Footer,
     HomeLayout,
+  },
+  mounted() {
+    if (!this.$store.hasModule('calculator')) {
+      this.$store.registerModule('calculator', calculatorModule());
+    }
+    if (!this.$store.hasModule('patient')) {
+      this.$store.registerModule('patient', patientModule());
+    }
   },
 };
 </script>
